2436882437f34f6e28c080a4ea7a7c5326d51c08
theMstafa <mm22180481@gmail.com>
2023-11-29 19:16:49 +0300

    pushing

--- APIs/urls.py
+++ APIs/urls.py
@@ -4,6 +4,6 @@ from .views import  *
 urlpatterns = [
     path('bedaia',BedaiaListAPIView.as_view(),name='bedaias'),
     path('bedaia/<str:slug>',BedaiaDetailsAPIView.as_view(),name='bedaia'),
-     path('Nukhba',NukhbaListAPIView.as_view(),name='Nukhba'),
-    path('Nukhba/<str:slug>',NukhbaDetailsAPIView.as_view(),name='Nukhba'),   
+     path('nukhba',NukhbaListAPIView.as_view(),name='Nukhba'),
+    path('nukhba/<str:slug>',NukhbaDetailsAPIView.as_view(),name='Nukhba'),   
 ]
