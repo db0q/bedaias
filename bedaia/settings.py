991c185ba8fb36dabea0d4656b06ad3403caa867
theMstafa <mm22180481@gmail.com>
2023-11-30 18:16:32 +0300

    fixing

--- bedaia/settings.py
+++ bedaia/settings.py
@@ -157,4 +157,4 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+AWS_S3_CUSTOM_DOMAIN = 'd2h89xp14a2787.cloudfront.net'
